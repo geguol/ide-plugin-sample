dd7e207aa8384508de1940825ee47343cae126ef
GitHub Action <action@github.com>
2022-03-21 02:29:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/geguol/idepluginsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/geguol/idepluginsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.geguol.idepluginsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.geguol.idepluginsample.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/geguol/idepluginsample/services/MyProjectService.kt
+++ src/main/kotlin/com/github/geguol/idepluginsample/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.geguol.idepluginsample.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.geguol.idepluginsample.MyBundle
 
 class MyProjectService(project: Project) {
 
